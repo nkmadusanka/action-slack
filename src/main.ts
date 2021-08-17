a60f5df19acdd0f3abc20a822071e96254c0e789
Niroshana Madusanka <nkmadusanka@gmail.com>
2021-08-17 16:05:31 +0200

    slack template as a string

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,9 @@ async function run(): Promise<void> {
         await client.send(await client.prepare(text));
         break;
       case Custom:
-        await client.send(await client.custom(custom_payload));
+        const pl = await client.custom(custom_payload);
+        core.info(pl);
+        await client.send(pl);
         break;
       default:
         throw new Error(
