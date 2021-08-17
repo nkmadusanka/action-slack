2600ab4321885b758257f38ba6f4cf6874963613
Niroshana Madusanka <nkmadusanka@gmail.com>
2021-08-17 17:09:59 +0200

    slack template as a string

--- src/client.ts
+++ src/client.ts
@@ -82,9 +82,9 @@ export class Client {
   async custom(payload: string) {
     await this.fieldFactory.attachments();
     /* eslint-disable no-var */
-    // var template = eval(`template = ${payload}`);
+    var template = eval(`template = ${payload}`);
     /* eslint-enable */
-    return payload;
+    return template;
   }
 
   async prepare(text: string) {
@@ -95,7 +95,7 @@ export class Client {
   }
 
   async send(payload: string | IncomingWebhookSendArguments) {
-    core.debug(JSON.stringify(context, null, 2));
+    core.info(JSON.stringify(payload, null, 2));
     await this.webhook.send(payload);
     core.debug('send message');
   }
